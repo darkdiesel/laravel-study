182225ead93909aa4c7be7c3f4b4aae378b23db2
Igor Peshkov <igor.peshkov@gmail.com>
2019-01-31 17:28:17 +0300

    Create setters for password in User model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    function setPasswordAttribute($password){
+        if ($password){
+            $this->attributes['password'] = bcrypt($password);
+        }
+    }
 }
