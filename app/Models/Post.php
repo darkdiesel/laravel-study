e3cab9570b334a44a5c43f6b6f11c63496638866
Igor Peshkov <igor.peshkov@gmail.com>
2019-01-31 17:03:35 +0300

    Set fillable columns for post model.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ class Post extends Model
 {
     protected $table = 'posts';
     protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'description', 'user_id'];
 }
